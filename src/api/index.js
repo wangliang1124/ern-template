f10c92c4c2764fb04109b85cac46462483dc800c
Liang <wangliang1124@163.com>
2021-04-09 22:05:04 +0800

    add fetchImages method to fetch photos from web

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import BaseURL, { PIXABAY_API_KEY } from '~/config';
+import { formatParams } from '~/utils';
+import Request from '~/utils/request';
 
 // 这个主要存放接口方法，可以把 Native 接口或 restful 接口都统一放在这里，根据项目的复杂程度，可以按 feature 分多个文件存放
-export default {
-  demo: {
+// 对于异步网络请求的方法，使用 fetchSomethings 的命名方式
+const Api = {
+  Demo: {
     fetchTodos() {
       return new Promise((resolve) => {
         setTimeout(async () => {
@@ -19,7 +23,19 @@ export default {
         }, 1000);
       });
     },
+    fetchImages({ q = '', page = 1, perPage = 20 }) {
+      const params = {
+        q,
+        page,
+        per_page: perPage,
+      };
+      const url = `${BaseURL.Pixabay}?key=${PIXABAY_API_KEY}&${formatParams(params)}`;
+
+      return Request.get(url);
+    },
   },
   refundAlert: {},
   sift: {},
 };
+
+export default Api;
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export const BaseURL = {
+  Pixabay: 'https://pixabay.com/api/',
+};
+
+export const PIXABAY_API_KEY = '21076284-3c0b1649eaf143ee6c172c9ff';
+
+export default BaseURL;

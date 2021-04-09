b69951d5915927f553d7e8f9f9665f34439eb1c9
Liang <wangliang1124@163.com>
2021-04-09 21:56:18 +0800

    move app.js to src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import Gallery from '~/screens/Gallery';
 import Home from '~/screens/Home';
 import MobxDemo from '~/screens/MobxDemo';
 
 const Stack = createStackNavigator();
-const routes = ['MobxDemo'];
+const routes = ['MobxDemo', 'Gallery'];
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home">{(props) => <Home {...props} routes={routes} />}</Stack.Screen>
         <Stack.Screen name="MobxDemo" component={MobxDemo} />
+        <Stack.Screen name="Gallery" component={Gallery} />
       </Stack.Navigator>
     </NavigationContainer>
   );

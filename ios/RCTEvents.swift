794512308bd8b16ef84fc5adaf52d479bd045cb4
Liang <wangliang1124@163.com>
2021-04-14 22:14:48 +0800

    add RCTManager, RCTEvents

---
+++ ios/RCTEvents.swift
@@ -0,0 +1,17 @@
+//
+//  RCTEvents.swift
+//  ERNTemplate
+//
+//  Created by 王亮 on 2021/4/14.
+//
+
+import Foundation
+
+enum RCTEvents: String, CaseIterable {
+    case AppOpened
+    case EventReminder
+
+    static var all: [String] {
+        return allCases.map({ $0.rawValue })
+    }
+}
